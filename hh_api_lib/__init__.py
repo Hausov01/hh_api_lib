fd97358b739a9a120d6a45745539a02bc77c1672
Hausov01 <domashov.aleksey@gmail.com>
2026-04-04 22:29:55 +0300

    Исправлен init под прошлый коммит

--- hh_api_lib/__init__.py
+++ hh_api_lib/__init__.py
@@ -1,11 +1,10 @@
 from .client import external_request
-from .config import HHConfig, SearchParams_period, SearchParams_dateTodate
+from .config import HHConfig, SearchParams
 from .exceptions import HHCaptchaRequired
 
 __all__ = [
     "external_request",
     "HHConfig",
-    "SearchParams_period",
-    "SearchParams_dateTodate",
+    "SearchParams",
     "HHCaptchaRequired",
 ]
