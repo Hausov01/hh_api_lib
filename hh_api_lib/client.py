e285792868a6560074534fd73434b1f4052bb610
Hausov01 <domashov.aleksey@gmail.com>
2026-05-12 17:26:03 +0300

    stable

--- hh_api_lib/client.py
+++ hh_api_lib/client.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from typing import Any, Dict
 import math
 import logging
+from datetime import datetime, timedelta
 from typing import Optional
 from .config import HHConfig, SearchParams
 from .exceptions import HHCaptchaRequired
@@ -39,6 +40,7 @@ class HHCaptchaRequired(Exception):
         self.captcha_url = captcha_url
         super().__init__(f"Captcha required. Solve here: {captcha_url}")
 
+#Основная функция
 def external_request(search_config: SearchParams, show_progress: bool = False, get_total_found: bool = False):
         count = 0
         results = []
@@ -66,66 +68,56 @@ def external_request(search_config: SearchParams, show_progress: bool = False, g
                 return all_data
 
 
-
+#Сбор id вакансий и пагинация
 def _get_all_vacancy_ids(session, search_config: SearchParams, hhconfig: HHConfig):
 
-    all_vacancy_ids=[]
-    logger.info("Fetching vacancy pages info")
-    params = {
-        'text': search_config.vacancy,
-        'area': search_config.area_id,
-        'per_page': hhconfig.per_page,
-        'page': 0
-    }
+    start_date, end_date = _normalize_to_dates(search_config)
 
-    if search_config.period is not None:
-        params['period'] = search_config.period
+    all_ids = set()
 
-    elif search_config.start_date and search_config.end_date:
-        params['date_from'] = search_config.start_date
-        params['date_to'] = search_config.end_date
-
-    else:
-        raise ValueError("Provide either period or start_date + end_date")
-
-    data = _request(session, search_config.base_url, params, hhconfig.timeout)
-    total_found = data.get('found', 0)
-    pages_available = data.get('pages', 0)
-    logger.info(f"total_found:{total_found}, pages_available: {pages_available}")
-    total_found = min(total_found, hhconfig.max_vacancies)
-    pages_available = math.ceil(total_found / hhconfig.per_page)
-    logger.info(f"After max_vacancies debug total_found:{total_found}, pages_available: {pages_available}")
-    seen = set()
-    for i in range(pages_available):
-        logger.info("page %s", i)
+    def _collect_recursive(date_from: str, date_to: str):
+        #корректировка формата дат
+        dt_from = _parse_date(date_from)
+        dt_to = _parse_date(date_to)
         params = {
             'text': search_config.vacancy,
             'area': search_config.area_id,
             'per_page': hhconfig.per_page,
-            'page': 0
+            'page': 0,
+            'date_from': _format_date(dt_from),
+            'date_to': _format_date(dt_to)
         }
-
-        if search_config.period is not None:
-            params['period'] = search_config.period
-
-        elif search_config.start_date and search_config.end_date:
-            params['date_from'] = search_config.start_date
-            params['date_to'] = search_config.end_date
-
-        else:
-            raise ValueError("Provide either period or start_date + end_date")
-
         data = _request(session, search_config.base_url, params, hhconfig.timeout)
-        vacancy_ids = [item['id'] for item in data.get('items', []) if 'id' in item]
-        logger.debug("Fetched IDs: %s", vacancy_ids)
-        for item in vacancy_ids:
-            #if len(all_vacancy_ids) >= hhconfig.max_vacancies:
-            #    return all_vacancy_ids
-            if item not in seen:
-                seen.add(item)
-                all_vacancy_ids.append(item)
-    return all_vacancy_ids, total_found
+        total_found = data.get('found', 0)
+        pages = data.get('pages', 0)
+        logger.info("Range %s → %s | found=%s", date_from, date_to, total_found)
 
+        # Если превышает лимит API — делим диапазон и рекурсивно запрашиваем по новому
+        if total_found >= hhconfig.max_vacancies:
+            # защита от бесконечного деления
+            if (dt_to - dt_from).days <= 1:
+                logger.warning("Too small interval, but still too many results: %s → %s", date_from, date_to)
+            mid = dt_from + (dt_to - dt_from) / 2
+            mid_str = _format_date(mid)
+            _collect_recursive(date_from, mid_str)
+            _collect_recursive(mid_str, date_to)
+            return
+
+        # обычная пагинация
+        for page in range(pages):
+            params['page'] = page
+            data = _request(session, search_config.base_url, params, hhconfig.timeout)
+            vacancy_ids = [
+                item['id'] for item in data.get('items', [])
+                if 'id' in item
+            ]
+            all_ids.update(vacancy_ids)
+
+    _collect_recursive(start_date, end_date)
+    logger.info("Total unique IDs: %s", len(all_ids))
+    return list(all_ids), len(all_ids)
+
+#Функция запроса информации о конкретной вакансии
 def _get_all_vacancy_details(session, all_vacancy_ids, search_config: SearchParams, hhconfig: HHConfig, show_progress: bool = False,):
     all_data = []
     iterator = all_vacancy_ids
@@ -145,15 +137,15 @@ def _get_all_vacancy_details(session, all_vacancy_ids, search_config: SearchPara
     return all_data
 
 
-
-
+#Вспомогательные функции
+#Функция для обработки запросов в API
 def _request(session: requests.Session, url: str, params: dict | None, timeout, max_retries: int = 3) -> Dict[str, Any]:
     for attempt in range(max_retries):
         try:
             response = session.get(url, params=params, timeout=timeout)
 
-            # --- 429 ---
-            if response.status_code == 429:
+            # код ошибки 429//502
+            if response.status_code == 429 or response.status_code == 502:
                 sleep_time = 2 ** attempt
                 logger.info(f"Rate limit hit. Retry in {sleep_time}s")
                 time.sleep(sleep_time)
@@ -173,23 +165,44 @@ def _request(session: requests.Session, url: str, params: dict | None, timeout, 
             return data
 
         except requests.exceptions.Timeout:
-            logger.error(f"Timeout on attempt {attempt+1}")
+            logger.error(f"Timeout on attempt {attempt+1}/{max_retries}")
         except requests.exceptions.ConnectionError:
-            logger.error(f"Connection error on attempt {attempt+1}")
+            logger.error(f"Connection error on attempt {attempt+1}/{max_retries}")
         except requests.exceptions.HTTPError as e:
             status = e.response.status_code
 
             if status == 403:
                 logger.warning(f"Access denied (403) for {url}")
                 return None
+            if status == 404:
+                logger.warning(f"Not found (404) url: {url}")
+                return None
 
             logger.error(f"HTTP error: {e}")
-            raise
         except ValueError:
             logger.error("Invalid JSON response")
-            raise
-
 
         time.sleep(2 ** attempt)
 
-    raise RuntimeError("Max retries exceeded")
+    logger.warning(f"Max retries ({max_retries}) exceeded for {url}. Skipping...")
+    return None
+
+#Функция для превращения периода в конкретные даты границы для сбора данных
+def _normalize_to_dates(search_config):
+    if search_config.start_date and search_config.end_date:
+        return search_config.start_date, search_config.end_date
+
+    if search_config.period is not None:
+        end = datetime.now()
+        start = end - timedelta(days=search_config.period)
+
+        return start.isoformat(), end.isoformat()
+
+    raise ValueError("Provide either period or start_date + end_date")
+
+def _parse_date(date_str: str) -> datetime:
+    return datetime.strptime(date_str, "%d.%m.%Y")
+
+
+def _format_date(dt: datetime) -> str:
+    return dt.strftime("%d.%m.%Y")
